77c0744f6061cd8efe2fdb5995a648ec51e32107
ismaelcmajada <ismaelcmajada@gmail.com>
2024-11-11 16:47:02 +0000

    Elimina las reglas personalizadas de validación para 'dni' y 'telephone' en el modelo Suscriptor

--- app/Models/Suscriptor.php
+++ app/Models/Suscriptor.php
@@ -30,7 +30,6 @@ class Suscriptor extends Model
                 'form' => true,
                 'rules' => [
                     'required' => true,
-                    'custom' => ['dni', 'telephone']
                 ]
             ],
             [
@@ -138,20 +137,4 @@ class Suscriptor extends Model
     protected static $forbiddenActions = [];
 
     protected static $calendarFields = [];
-
-    public static function getCustomRules()
-    {
-        return [
-            'dni' => function ($attribute, $value, $fail) {
-                if (!preg_match('/^\d{8}[A-Z]$/', $value)) {
-                    $fail('El DNI debe tener 8 números seguidos de una letra mayúscula.');
-                }
-            },
-            'telephone' => function ($attribute, $value, $fail) {
-                if (!preg_match('/^\d{8,15}$/', $value)) {
-                    $fail('El teléfono debe tener entre 8 y 15 números.');
-                }
-            }
-        ];
-    }
 }
