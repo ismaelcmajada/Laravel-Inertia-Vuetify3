430327dc960e0d186409bc16e5583577b34fa5d1
ismaelcmajada <ismaelcmajada@gmail.com>
2025-12-13 23:15:01 +0000

    Forzar HTTPS en producción.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,5 +22,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Schema::defaultStringLength(191);
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        };
     }
 }
